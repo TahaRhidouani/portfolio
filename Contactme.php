da4354d8c114f8bc12d139be12173aaca842e99a
Taha <taha.rhidouani@gmail.com>
2021-03-21 20:13:50 -0400

    Sanitize input for email and small UI changes

--- Contactme.php
+++ Contactme.php
@@ -2,11 +2,11 @@
     require_once ("assets/phpmailer/PHPMailerAutoload.php");
     
     if(isSet($_POST['status']) && $_POST['status'] == 'submit' && isSet($_POST['firstname']) && $_POST['firstname'] != '' && isSet($_POST['lastname']) && $_POST['lastname'] != '' && isSet($_POST['email']) && $_POST['email'] != '' && isSet($_POST['subject']) && $_POST['subject'] != '' && isSet($_POST['message']) && $_POST['message'] != ''){
-        $firstname=$_POST['firstname'];
-        $lastname=$_POST['lastname'];
-        $email=$_POST['email'];
-        $subject=$_POST['subject'];
-        $msg=$_POST['message'];
+        $firstname=filter_var($_POST['firstname'], FILTER_SANITIZE_STRING);
+        $lastname=filter_var($_POST['lastname'], FILTER_SANITIZE_STRING);
+        $email=filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+        $subject=filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+        $msg=filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
         $mail = new PHPMailer(true);
 
@@ -18,7 +18,7 @@
             $mail->Port = '465';
             $mail->IsHTML();
             $mail->Username = 'taha.rhidouani@gmail.com';
-            $mail->Password = '***********';
+            $mail->Password = 'uosewaatqzxjbyun';
             $mail->SetFrom('no-reply@taharhidouani.com');
             $mail->Subject = $subject;
             $mail->Body = "<Strong>Name:</Strong> ".$firstname." ".$lastname."<br><Strong>Subject:</Strong> ".$subject."<br><Strong>Email:</Strong> ".$email."<br><br><Strong>Message:</Strong><br>".$msg;
